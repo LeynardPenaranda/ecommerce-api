943b35114788c4e5622347ed922114d366428a94
LeynardPenaranda <leynardlove@gmail.com>
2025-08-09 09:41:09 +0800

    fix

--- api/items.js
+++ api/items.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const items = [
     // COMPUTER PARTS (10)
     {
@@ -324,4 +324,4 @@ export default function handler(req, res) {
   }
 
   res.status(200).json(results);
-}
+};
